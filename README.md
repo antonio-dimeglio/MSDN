39c8106533f888b6c13a37284be465f633d28bb2
Antonio Di Meglio <antoniodimeglioic@gmail.com>
2024-05-18 17:21:09 +0200

    Added data generation

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # MSDN
+
+A reimplementation of the Mixed-Scale Dense Neural Network (MSDNet) in PyTorch.
+
+## Usage
+The conda environent can be created using the provided `environment.yml` file:
+
+```bash
+conda env create -f environment.yml
+```
+
+Which creates an environment named `msdn`. To activate the environment, run:
+
+```bash
+conda activate msdn
+```
+
+To generate a dataset, first run the phantom generation script:
+
+```bash
+python data_generation/phantom_generation.py
+```
+
+Then, run the sinogram generation script:
+
+```bash
+python data_generation/sinogram_generation.py
+```
+
+Finally, run the training script:
+
+```bash
+python train.py
+```
---
+++ data_generation/sinogram_generation.py
@@ -0,0 +1,41 @@
+import astra 
+import numpy as np
+import imageio.v2 as iio
+from pathlib import Path
+from time import time
+import argparse
+import os  
+import matplotlib.pyplot as plt
+
+def generate_sinograms():
+    folders = ['train', 'test', 'val']
+    image_type = ['noisy', 'clean', 'label']
+
+    for folder in folders:
+
+        # Create folder for sinograms
+        sinogram_folder = Path(f'sinogram/{folder}')
+        os.makedirs(sinogram_folder, exist_ok=True)
+        for img_type in image_type:
+            sinogram_type_folder = sinogram_folder / img_type
+            os.makedirs(sinogram_type_folder, exist_ok=True)
+
+            num_images = len(os.listdir(f'phantom/{folder}/{img_type}'))
+
+            for i in range(num_images):
+                data = iio.imread(f'phantom/{folder}/{img_type}/{i}.tiff')
+                proj_geom = astra.create_proj_geom('parallel', 1.0, data.shape[0], np.linspace(0, np.pi, data.shape[0], False))
+                vol_geom = astra.create_vol_geom(data.shape)
+                proj_id = astra.create_projector('linear', proj_geom, vol_geom)
+                id, sinogram = astra.create_sino(data, proj_id)
+
+                sinogram = sinogram / sinogram.max()
+
+                iio.imwrite(sinogram_type_folder / f'{i}.tiff', sinogram)
+                print(f'Sinogram {i} of {folder}/{img_type} generated.')
+
+def __main__():
+    generate_sinograms()
+
+if __name__ == '__main__':
+    __main__()
