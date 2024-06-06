2b098c3478a67af3a68008b9a9ce22cd32d24f96
Jan Żuromski <zuromskijan@gmail.com>
2024-06-06 17:05:51 +0200

    Modified 3D data generation to use several (5) phantoms at a time. Removed get_phantom for a more logical workflow.

--- data_generation/generate_all3d.py
+++ data_generation/generate_all3d.py
@@ -1,8 +1,8 @@
 import argparse
+from datetime import datetime
 import imageio.v2 as iio
 import numpy as np
 import os
-from tqdm import trange
 import re
 
 from phantom_3d import generate_phantom
@@ -19,28 +19,8 @@ def parse_phantom_filename(filename):
     return np.array(shapes)
 
 
-def get_phantom(data_folder, phantom_filepath, phantom_shape):
-    phantom = None
-    if phantom_shape is None and phantom_filepath is None:
-        print('Either filepath to phantom or desired phantom shape needed.')
-        return None
-    
-    if phantom_shape == None:
-        phantom_shape = parse_phantom_filename(phantom_filepath)
-
-    if phantom_filepath is None:
-        out_file  = (f'{data_folder}/phantom_{phantom_shape[0]}'
-                     f'_{phantom_shape[1]}_{phantom_shape[2]}.npy')
-        phantom = generate_phantom(phantom_shape, 1000, 1000, 50, save=True, 
-                                   out_file=out_file)
-    else: 
-        phantom = load_phantom(phantom_filepath, phantom_shape)
-
-    return phantom
-
-
-def generate_data(data_folder='3d_data', phantom_filepath = None,
-                  phantom_shape=None, group='clean'):
+def generate_data(data_folder='3d_data', phantom_shape=None,
+                  generate_phantoms=False, group='clean'):
     # Stage 0: Initialize file structure
     [os.makedirs(f'{data_folder}/{dt}/{split}/{angle}/{group}', exist_ok=True)
      for dt in ('phantoms', 'sinograms', 'fbps')
@@ -48,15 +28,36 @@ def generate_data(data_folder='3d_data', phantom_filepath = None,
      for angle in (45, 90, 180)]
     
     # Stage 1: Obtain phantom and slice it
-    phantom = get_phantom(data_folder, phantom_filepath, phantom_shape)
+    phantoms = []
+    phantom = None
+    if generate_phantoms:
+        for i in range(5):
+            datetime_pattern = datetime.now().strftime('%y%m%d%H%M%S')
+            out_file  = (f'{data_folder}/source_phantoms/phantom_'
+                         f'{phantom_shape[0]}_{phantom_shape[1]}_'
+                         f'{phantom_shape[2]}-{datetime_pattern}.npy')
+            p = generate_phantom(phantom_shape, 1000, 1000, 50,
+                                 save=True, out_file=out_file)
+            phantoms.append(p)
+    
+    else:
+        phantom_folder = f'{data_folder}/source_phantoms'
+        for fn in os.listdir(phantom_folder):
+            full_fn = f'{phantom_folder}/{fn}'
+            phantom_shape = parse_phantom_filename(fn)
+            p = load_phantom(full_fn, phantom_shape)
+            phantoms.append(p)
+        
+    phantom = np.concatenate(phantoms, axis=2)
+    print(f'Obtained 3D phantom of size {phantom.shape}.')
     slices = get_slices(phantom, rgba=False)
 
     # Stage 2: Split intro train/test/val
     shuffled_idx = np.arange(len(slices))
     np.random.shuffle(shuffled_idx)
-    # 60:30:10 split
+    # 70:30:0 split
     pivots = np.array(
-        [0.6 * len(shuffled_idx), 0.9 * len(shuffled_idx)]
+        [0.7 * len(shuffled_idx), len(shuffled_idx)]
         ).astype(int)
     split_idx = {'train': shuffled_idx[:pivots[0]], 
                  'test': shuffled_idx[pivots[0]:pivots[1]],
@@ -101,15 +102,16 @@ def main():
                               'the data should be stored.'))
     parser.add_argument('--phantom_filename', '-pf', required=False,
                         help='The path to the pre-generated phantom.')
-    parser.add_argument('--phantom_shape', '-ps', type=int, nargs=3,
-                        help='an integer for the accumulator')
+    parser.add_argument('--generate_phantoms', '-gp', action='store_true',
+                        help=('When called, the program '
+                              'generates source phantoms first.'))
     parser.add_argument('--noisy', '-n', action='store_const', const='noisy',
                         default='clean',
-                        help='an integer for the accumulator')
+                        help='When called, noise is applied to slices.')
     args = parser.parse_args()
-    phantom_shape = np.array(args.phantom_shape)
-    generate_data(args.data_folder, args.phantom_filename, phantom_shape,
-                  group=args.noisy)
+    phantom_shape = np.array([256, 256, 128])
+    generate_data(args.data_folder, phantom_shape,
+                  args.generate_phantoms, group=args.noisy)
     
 
 if __name__ == '__main__':
