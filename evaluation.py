51c63235c771a7034eb4212a198db920376db911
Jan Żuromski <zuromskijan@gmail.com>
2024-06-07 21:10:18 +0200

    Include SSIM calculation. Include nan checking.

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,10 @@ import imageio.v2 as iio
 import numpy as np
 import os
 import torch
-import torch.nn as nn
-from torchsummary import summary
 from torch.utils.data import DataLoader
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import mean_squared_error as mse
+from tqdm import tqdm
 
 from src.MSDNDataset import MSDNDataset
 from src.MSDNNet import MSDNet
@@ -19,8 +18,8 @@ def get_test_dataloaders(root_folder,
 
 
     datasets = {
-        group: {angle: MSDNDataset(f'{root_folder}/recon/test/{group}/{angle}',
-                                   f'{root_folder}/phantom/test/{group}/{angle}',
+        group: {angle: MSDNDataset(f'{root_folder}/recon/test/{angle}/{group}',
+                                   f'{root_folder}/phantom/test/{angle}/{group}',
                                    transform)
                 for angle in num_angles} 
         for group in groups
@@ -34,23 +33,19 @@ def get_test_dataloaders(root_folder,
 
 
 def main():
-    model_fn = 'model2.pth'
+    root_folder = '3d_data'
+    model_fn = 'model3d.pth'
     timestamp = datetime.now().strftime('%y%m%d%H%M%S')
     eval_dir = f"evaluations/{model_fn.replace('.pth', '')}_{timestamp}"
     os.makedirs(eval_dir)
     print('Loading model.')
-    model = MSDNet(in_channels=4, 
-                   out_channels=4, 
-                   num_features=4, 
-                   num_layers=100,  
-                   dilations=np.arange(1, 101))
-    
+    model = MSDNet(in_channels=4, out_channels=4, num_features=4, 
+                   num_layers=100,  dilations=np.arange(1, 101))
     model.load_state_dict(torch.load(model_fn,
                                      map_location=torch.device('cpu')))
-    # summary(model)
     print('Model loaded succesfully.\nLoading datasets...')
 
-    dataloaders = get_test_dataloaders('.', None)
+    dataloaders = get_test_dataloaders(root_folder, None)
     print('Datasets loaded succesfully.')
 
     metrics = {
@@ -58,7 +53,7 @@ def main():
         'rmse': {'msdn': 0, 'fbp': 0}
     }
     counter = 0
-    for inputs, targets in dataloaders['clean'][45]:
+    for inputs, targets in tqdm(dataloaders['clean'][45]):
         if inputs.size()[0] % 4 != 0:
             break
         outputs = model(inputs)
@@ -66,24 +61,31 @@ def main():
 
         for i in range(len(outputs)):
             ground_truth = iio.imread(
-                f'phantom/test/clean/45/{counter * 4 + i}.tiff')
+                f'{root_folder}/phantom/test/45/clean/{counter * 4 + i}.tiff')
             output = outputs[i].detach().numpy()
             input = inputs[i].detach().numpy()
-            metrics['ssim']['msdn'] += ssim(ground_truth, output,
-                                            data_range=output.max() - output.min())
+            ssim_msdn = ssim(ground_truth, output,
+                             data_range=output.max() - output.min())
+            if np.isnan(ssim_msdn):
+                ssim_msdn = metrics['ssim']['msdn'] / (counter * 4 + i)
+            metrics['ssim']['msdn'] += ssim_msdn
             metrics['rmse']['msdn'] += np.sqrt(mse(output, ground_truth))
+            ssim_fbp = ssim(ground_truth, input,
+                            data_range=input.max() - input.min())
+            if np.isnan(ssim_fbp):
+                ssim_fbp = metrics['ssim']['fbp'] / (counter * 4 + i)
+            metrics['ssim']['fbp'] += ssim_fbp
+            metrics['rmse']['fbp'] += np.sqrt(mse(input, ground_truth))
             
             iio.imsave(f'{eval_dir}/output_{counter * 4 + i}.tiff', output)
             iio.imsave(f'{eval_dir}/input_{counter * 4 + i}.tiff', input)
         counter += 1
     
-    metrics['rmse']['msdn'] /= len(dataloaders['clean'][45])
-    metrics['ssim']['msdn'] /= len(dataloaders['clean'][45])
-    print(len(dataloaders['clean'][45]))
+    length = counter * 4
     for metric in metrics.keys():
         print(metric.upper(), ':')
         for model in metrics[metric].keys():
-            print(f"\t{model.upper()}: {metrics[metric][model]:.4f}")
+            print(f"\t{model.upper()}: {metrics[metric][model]/length:.4f}")
 
 
 
